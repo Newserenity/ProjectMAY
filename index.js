f6dfb5d97cb757675d36d28cd40810e06a400b4d
(Newserenity) <(newcephei@icloud.com)>
2020-05-03 00:02:30 +0900

    0.1.2

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
-let folderOpenVariable1 = true;
-let folderOpenVariable2 = true;
-let folderOpenVariable3 = true;
+let opendFold = [];
 
-function foldOpen1(){
+function foldOpen(id) {
+    var index = opendFold.indexOf(id);
 
-    // let foldDiv1 = document.getElementById("foldDiv1");
-    // foldDiv1.classList.add(foldDiv)
-
-    if (folderOpenVariable1){
-        document.getElementById("foldDiv1").className = "text-filde-div-show";
-        folderOpenVariable1 = false;
+    if( -1 >= index) {
+        document.getElementById(id).className = "text-filde-div-show";
+        opendFold.push(id);
     } else {
-        document.getElementById("foldDiv1").className = "text-filde-div";
-        folderOpenVariable1 = true;
-    }
-}
-
-function foldOpen2(){
-
-    if (folderOpenVariable2){
-        document.getElementById("foldDiv2").className = "text-filde-div-show";
-        folderOpenVariable2 = false;
-    } else {
-        document.getElementById("foldDiv2").className = "text-filde-div";
-        folderOpenVariable2 = true;
-    }
-}
-
-function foldOpen3(){
-
-    if (folderOpenVariable3){
-        document.getElementById("foldDiv3").className = "text-filde-div-show";
-        folderOpenVariable3 = false;
-    } else {
-        document.getElementById("foldDiv3").className = "text-filde-div";
-        folderOpenVariable3 = true;
+        document.getElementById(id).className = "text-filde-div";
+        opendFold.splice(index, 1);
     }
 }
 
@@ -47,6 +21,7 @@ function addItem(){
     }
 
     const listElement = document.getElementById("todoList");
+    const doingList = document.getElementById("doingList");
 
     const div = document.createElement("div");
     const head = document.createElement("p");
@@ -64,13 +39,22 @@ function addItem(){
     head.appendChild(inputFildeValue);
 
     date.innerHTML = month + "/" + day;
-    moveButton.innerHTML = "削除";
-    removeButton.innerHTML = "移動";
+    moveButton.innerHTML = "移動";
+    removeButton.innerHTML = "削除";
 
     head.classList.add("event-content");
     date.classList.add("event-content");
     moveButton.classList.add("del-button");
     removeButton.classList.add("del-button");
+
+    removeButton.addEventListener("click", function () {
+        div.remove()
+    });
+
+    moveButton.addEventListener("click", function(){
+        div.appendChild(doingList);
+        div.remove();
+    });
 
     div.appendChild(head);
     div.appendChild(date);
@@ -87,3 +71,12 @@ function delItem(){
 function moveItem(){
 
 }
+
+// var input = document.getElementById("myInput");
+
+//     input.addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//     event.preventDefault();
+//     document.getElementById("myBtn").click();
+//   }
+// }); 
